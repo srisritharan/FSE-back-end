433761c1e8af2c6d5b18a0a2975550be2c3572b0
Sritharan_Srinivasan <srisritharan.s@gmail.com>
2024-04-28 19:46:27 -0400

    initial server with dummy endpoints + morgan log

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ import queriesRouter from './routes/queries/queries.js';
 import dotenv  from "dotenv"; // to load in the environment vars
 dotenv.config();
 import log from './utlis/log.js'
+import morgan from "morgan"
 
 const app = express(); //creates an express app object
 const port = process.env.PORT || 4000; // use env var or default to 4000
 
 app.use(bodyParser.json());
+
+// Log ever request which comes in using morgan. https://github.com/expressjs/morgan
+//:method :url :status :response-time ms - :res[content-length]
+app.use(morgan('dev'));
 
 app.use('/news', newsRouter);
 app.use('/users', usersRouter);
